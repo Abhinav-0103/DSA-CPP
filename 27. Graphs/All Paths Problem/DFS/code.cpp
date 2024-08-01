d57682680fb0c673a0510f1f99b72f6068d9c5e7
Abhinav <abhinava808@gmail.com>
2024-08-01 13:56:30 +0530

    Till All Paths Problem

---
+++ 27. Graphs/All Paths Problem/DFS/code.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+#include<list>
+#include<vector>
+#include<queue>
+#include<string>
+using namespace std;
+
+class Graph {
+    int V;
+    list<int>* l;
+
+    public :
+        Graph(int V) {
+            this->V = V;
+            l = new list<int> [V];
+        }
+
+        void add_edge(int u, int v) {
+            l[u].push_back(v);
+        }
+
+        void allPathsUtil(int src, int dest, vector<bool> &vis, string &path) {
+            if(src == dest) {
+                cout << path << dest << endl;
+                return;
+            }
+
+            vis[src] = true;
+            path.push_back(to_string(src)[0]);
+
+            for(int v: l[src]) {
+                if(!vis[v]) {
+                    vis[v] = true;
+                    allPathsUtil(v,dest,vis,path);
+                    vis[v] = false;
+                }
+            }
+
+            path.pop_back();
+        }
+
+        void allPaths(int src, int dest) {
+            vector<bool> vis(V, false);
+            string path = "";
+            allPathsUtil(src,dest,vis,path);
+        }
+};
+
+int main() {
+    Graph graph(6);
+
+    graph.add_edge(0,3);
+
+    graph.add_edge(2,3);
+
+    graph.add_edge(3,1);
+
+    graph.add_edge(4,0);
+    graph.add_edge(4,1);
+
+    graph.add_edge(5,0);
+    graph.add_edge(5,2);
+
+
+    graph.allPaths(5,1);
+
+    return 0;
+}
